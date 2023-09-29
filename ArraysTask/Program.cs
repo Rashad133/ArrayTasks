19428d44c6acb42f94719028a6e700dac8428f14
Rashad133 <tu82907hw@code.edu.az>
2023-09-29 05:58:49 +0400

    task is done

---
+++ ArraysTask/Program.cs
@@ -0,0 +1,33 @@
+﻿//Task 1
+
+//int[] arr = { 15, 16, 17, 18, 20 };
+//int sum = 0;
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    sum += arr[i];
+
+//}
+//Console.WriteLine(+sum);
+
+
+
+
+//Task 2
+
+//string[] arr = { "Salam", "Yeni", "Tasklar", "Geldi" };
+//for(int i=0; i<arr.Length; i++)
+//{
+//    if (arr[i].Length > 4)
+//    {
+//        Console.WriteLine(arr[i]);
+//    }
+
+//}
+
+
+
+
+
+
+
+
